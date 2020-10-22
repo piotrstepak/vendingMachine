7e91cd267897a7364c68ea5797c34b2e7f6207a1
pietia <piotrstepak@gmail.com>
2020-10-22 21:30:56 +0200

    Change overrided toString method return

--- models/coins/coin.js
+++ models/coins/coin.js
@@ -5,25 +5,9 @@ class Coin {
         this.diameter = diameter;
         this.mass = mass;
         Coin.prototype.toString = () => {
-            return this.name;
+            return `${this.name} - $${this.value}`;
         }
     }
-
-    // coinToString() {
-    //     Coin.prototype.toString = () => {
-    //         return this.name;
-    //     }
-    // }
-
-    // get diameter() {
-    //     return this.diameter;
-    // }
-
-    // get mass() {
-    //     return this.mass;
-    // }
-
-    // dlaczego takie getery nie dzialaja Cannot set property diameter of #<Coin> which has only a getter ? 
 }
 
 module.exports = Coin;
